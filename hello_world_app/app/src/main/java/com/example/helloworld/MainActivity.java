8eed927733d8463990521d17f5f4d10c03a93419
vungocson <vungocson998@gmail.com>
2019-09-07 16:25:19 +0700

    Code some button services

--- hello_world_app/app/src/main/java/com/example/helloworld/MainActivity.java
+++ hello_world_app/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**Call when button CLEAR is pressed**/
+    public void clearInput (View v){
+        TextView usd = (TextView) findViewById(R.id.INPUTUSD);
+        TextView eur = (TextView) findViewById(R.id.INPUTEUR);
+        TextView vnd = (TextView) findViewById(R.id.INPUTVND);
+
+        usd.setText("");
+        eur.setText("");
+        vnd.setText("");
+    }
+
+    /**Call when button CONVERT is pressed**/
+    public void convertCurrencies (View v){
+        TextView usd = (TextView) findViewById(R.id.INPUTUSD);
+        TextView eur = (TextView) findViewById(R.id.INPUTEUR);
+        TextView vnd = (TextView) findViewById(R.id.INPUTVND);
+
+        String value_usd = usd.getText().toString();
+        float value = Float.valueOf(value_usd);
+
+        eur.setText(String.valueOf(value*0.91));
+        vnd.setText(String.valueOf(value*22000));
     }
 }
